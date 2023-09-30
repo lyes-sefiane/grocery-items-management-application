6ee63fb148ae9b532c6cf390c2d530c333fa61ac
lyes-s <lyes.sefiane@gmail.com>
2023-09-30 12:44:38 -0400

    [GIM REST API] Repository Integration Tests

--- grocery-items-management/src/test/java/com/lyess/groceryitems/GroceryItemsManagementApplicationTests.java
+++ grocery-items-management/src/test/java/com/lyess/groceryitems/GroceryItemsManagementApplicationTests.java
@@ -1,13 +1,5 @@
 package com.lyess.groceryitems;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class GroceryItemsManagementApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
