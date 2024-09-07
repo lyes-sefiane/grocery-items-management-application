6c30c9db15d9e7347daaac8b743c3b40cf43b9a5
lyes-s <lyes.sefiane@gmail.com>
2024-09-07 09:17:40 -0400

    Grocery Items Management Application

---
+++ grocery-items-management-desktop-ui/src/test/java/com/lyess/grocery_items_management_desktop_ui/GroceryItemsManagementDesktopUiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lyess.grocery_items_management_desktop_ui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroceryItemsManagementDesktopUiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
