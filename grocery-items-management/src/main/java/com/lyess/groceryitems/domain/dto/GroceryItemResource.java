981e94601b5235c0c6bf03d341fce869f4d2777f
lyes-s <lyes.sefiane@gmail.com>
2024-10-07 08:53:19 -0400

    Grocery Items Management
    - RESTful Webservice Update

--- grocery-items-management/src/main/java/com/lyess/groceryitems/domain/dto/GroceryItemResource.java
+++ grocery-items-management/src/main/java/com/lyess/groceryitems/domain/dto/GroceryItemResource.java
@@ -1,11 +1,7 @@
 package com.lyess.groceryitems.domain.dto;
 
-import com.lyess.groceryitems.controller.GroceryItemRestController;
 import com.lyess.groceryitems.domain.entity.GroceryItem;
-import org.springframework.hateoas.RepresentationModel;
-
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+import org.springframework.hateoas.Link;
 
 /**
  * Project Name : grocery-items-management-application
@@ -14,31 +10,4 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
  * User : Lyes Sefiane
  * Created : 2023-09-27 10:18 AM
  */
-public class GroceryItemResource extends RepresentationModel<GroceryItemResource> {
-
-    private final GroceryItem groceryItem;
-
-    public GroceryItemResource(GroceryItem groceryItem) {
-        this.groceryItem = groceryItem;
-        this.addLink(groceryItem);
-    }
-
-    /**
-     * Add Hateoas Links
-     */
-    private void addLink(GroceryItem groceryItem) {
-        this.add(linkTo(methodOn(GroceryItemRestController.class)
-                .findGroceryItem(groceryItem.getId()))
-                .withSelfRel());
-    }
-
-    /**
-     * Get Grocery Item
-     *
-     * @return groceryItem
-     */
-    public GroceryItem getGroceryItem() {
-        return groceryItem;
-    }
-
-}
+public record GroceryItemResource(GroceryItem groceryItem, Link link) {}
