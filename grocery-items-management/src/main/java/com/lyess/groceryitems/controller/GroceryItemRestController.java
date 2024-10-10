56c043d2108be147c05587213fd25bc09267c348
lyes-s <lyes.sefiane@gmail.com>
2024-10-10 14:28:07 -0400

    OpenApi/Swagger Integration

--- grocery-items-management/src/main/java/com/lyess/groceryitems/controller/GroceryItemRestController.java
+++ grocery-items-management/src/main/java/com/lyess/groceryitems/controller/GroceryItemRestController.java
@@ -3,12 +3,22 @@ package com.lyess.groceryitems.controller;
 import com.lyess.groceryitems.domain.dto.GroceryItemRecord;
 import com.lyess.groceryitems.domain.dto.GroceryItemResource;
 import com.lyess.groceryitems.domain.entity.GroceryItem;
+import com.lyess.groceryitems.exception.entities.ExceptionResponse;
 import com.lyess.groceryitems.service.IGroceryItemService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.hateoas.Link;
+import org.springframework.http.MediaType;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -24,9 +34,16 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
  * Created : 2023-09-24 1:14 PM
  */
 @RestController
-@RequestMapping("/v1/items")
+@RequestMapping(value = "/v1/items", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 @Validated
+@Tag(name = "Grocery Items", description = "Grocery Items Management REST Controller")
+@ApiResponses(value = {
+        @ApiResponse(responseCode = "404", description = "NOT_FOUND", content = @Content(schema = @Schema(implementation = ExceptionResponse.class))),
+        @ApiResponse(responseCode = "400", description = "BAD_REQUEST", content = @Content(schema = @Schema(implementation = ExceptionResponse.class))),
+        @ApiResponse(responseCode = "415", description = "UNSUPPORTED_MEDIA_TYPE", content = @Content(schema = @Schema(implementation = ExceptionResponse.class)))})
 public class GroceryItemRestController {
+
+    Logger logger = LoggerFactory.getLogger(GroceryItemRestController.class);
 
     private final IGroceryItemService groceryItemService;
 
@@ -45,7 +62,10 @@ public class GroceryItemRestController {
      * @see GroceryItemResource
      */
     @GetMapping
+    @Operation(summary = "Find Grocery Items")
+    @ApiResponse(responseCode = "200", description = "OK", content = @Content(schema = @Schema(implementation = GroceryItemResource.class)))
     public List<GroceryItemResource> findAllGroceryItems() {
+        logger.info("Find All Grocery Items ....");
         return groceryItemService.findAllGroceryItems().stream().map(groceryItem -> new GroceryItemResource(groceryItem, addLink(groceryItem))).toList();
     }
 
@@ -57,8 +77,11 @@ public class GroceryItemRestController {
      * @see GroceryItem
      * @see GroceryItemResource
      */
-    @RequestMapping("/{id}")
+    @GetMapping("/{id}")
+    @Operation(summary = "Find Grocery Item")
+    @ApiResponse(responseCode = "200", description = "OK", content = @Content(schema = @Schema(implementation = GroceryItemResource.class)))
     public GroceryItemResource findGroceryItem(@PathVariable @NotBlank(message = "Grocery Item Should Not Be Blank !") String id) {
+        logger.info("Find Grocery Items With The Following Id : {}", id);
         GroceryItem groceryItem = groceryItemService.findGroceryItem(id);
         return new GroceryItemResource(groceryItem, addLink(groceryItem));
     }
@@ -72,7 +95,10 @@ public class GroceryItemRestController {
      * @see GroceryItemResource
      */
     @PostMapping
+    @Operation(summary = "Register Grocery Item")
+    @ApiResponse(responseCode = "200", description = "OK", content = @Content(schema = @Schema(implementation = GroceryItemResource.class)))
     public GroceryItemResource saveGroceryItem(@RequestBody @Valid GroceryItemRecord groceryItemRecord) {
+        logger.info("Register The Following Grocery Item : {}", groceryItemRecord);
         GroceryItem groceryItem = groceryItemService.saveGroceryItem(customConverter.convert(groceryItemRecord));
         return new GroceryItemResource(groceryItem, addLink(groceryItem));
     }
@@ -87,8 +113,11 @@ public class GroceryItemRestController {
      * @see GroceryItemRecord
      */
     @PutMapping("/{id}")
+    @Operation(summary = "Update Grocery Item")
+    @ApiResponse(responseCode = "200", description = "OK", content = @Content(schema = @Schema(implementation = GroceryItemResource.class)))
     public GroceryItemResource updateGroceryItem(@RequestBody @Valid GroceryItemRecord groceryItemRecord, //
                                                  @PathVariable @NotBlank(message = "Grocery Id should Not Be Blank!") String id) {
+        logger.info("Update Grocery Item With The Following Id : {}, {}", id, groceryItemRecord);
         GroceryItem groceryItem = groceryItemService.updateGroceryItem(customConverter.convert(groceryItemRecord), id);
         return new GroceryItemResource(groceryItem, addLink(groceryItem));
     }
@@ -99,7 +128,10 @@ public class GroceryItemRestController {
      * @param id : Grocery Item Identifier.
      */
     @DeleteMapping("/{id}")
+    @Operation(summary = "Delete Grocery Item")
+    @ApiResponse(responseCode = "204", description = "NO_CONTENT", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE))
     public void deleteGroceryItem(@PathVariable @NotBlank(message = "Grocery Id should Not Be Blank!") String id) {
+        logger.info("Delete Grocery Item With The Following Id : {}", id);
         groceryItemService.deleteGroceryItem(id);
     }
 
